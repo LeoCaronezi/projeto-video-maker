17f4cc3e4d7ccee9d5cfbebe42de23eb8c0da9e3
LeoCaronezi <rafael.caronezi@outlook.com>
2019-03-12 00:58:32 -0300

    LeoCaronezi
    perguntar pelo termo de busca e
    qual prefixo do video

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const readline = require ('readline-sync')
 function start() {
     const content = {}
     content.searchTearm = askAndReturnSeachTerm()
+    content.prefix = askAndReturnPrefix()
     function askAndReturnSeachTerm () {
-        return 'TERM EXAMPLE'
+        return readline.question('Type a wikipedia search term:')
         
+    }
+    function askAndReturnPrefix() {
+        const prefixes = ['who is', 'what is', 'The history of ' ]
+         const selectedPrefixIndex = readline.keyInSelect(prefixes, 'choose one option:')
+         const selectPrefixText = prefixes[selectedPrefixIndex] 
+         
+        //  console.log(selectPrefixText)
+        return selectPrefixText      
     }
     console.log(content)
 }
